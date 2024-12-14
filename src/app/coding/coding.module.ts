606776e50c4af9d809b98153f8f026b5a887c98f
Natanael Neto <nesn.gamedev@gmail.com>
2024-12-14 19:15:40 -0300

    C5 - add Conways Game of Life

--- src/app/coding/coding.module.ts
+++ src/app/coding/coding.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { CodingComponent } from './index/coding.component';
 import { CodingRoutingModule } from './coding-routing.module';
 import { GameOfLifeComponent } from './game-of-life/game-of-life.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    CodingRoutingModule
+    CodingRoutingModule,
+    FormsModule
   ],
   declarations: [
     CodingComponent,
